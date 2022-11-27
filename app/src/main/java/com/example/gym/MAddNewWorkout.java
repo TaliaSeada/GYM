88d32b9727c13bc04836fbb568868caf7592ba0d
Talia Seada <talias1235@gmail.com>
2022-11-27 23:03:42 +0200

    firebase connection

--- app/src/main/java/com/example/gym/MAddNewWorkout.java
+++ app/src/main/java/com/example/gym/MAddNewWorkout.java
@@ -1,37 +1,14 @@
 package com.example.gym;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 public class MAddNewWorkout extends AppCompatActivity {
-
-    FirebaseDatabase rootNode;
-    DatabaseReference reference;
-
-    Button regBtn = findViewById(R.id.addWorkout);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m_add_workout);
 
-        //save data in FireBase on button click
-        regBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                rootNode = FirebaseDatabase.getInstance();
-                reference = rootNode.getReference("workout");
-
-
-
-                reference.setValue("change the workout data");
-            }
-        });
     }
 }
