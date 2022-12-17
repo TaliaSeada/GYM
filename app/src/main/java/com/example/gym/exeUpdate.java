2a96c52df105de23a7e623400b3a0f753ffb102b
Talia Seada <talias1235@gmail.com>
2022-12-17 22:37:53 +0200

    connect

--- app/src/main/java/com/example/gym/exeUpdate.java
+++ app/src/main/java/com/example/gym/exeUpdate.java
@@ -9,17 +9,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.MetadataChanges;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,15 +36,86 @@ public class exeUpdate extends AppCompatActivity {
     static String email;
     String Gworkout;
     DocumentReference docRef;
-    static double reps;
-    static double sets;
-    static double weight;
 
-    private static final String TAG = "DBWorkOut";
+    static Long reps = Long.valueOf(0);
+    static Long sets = Long.valueOf(0);
+    static double weight = 0;
+
+    private static final String TAG = "DBExercise";
     protected static FirebaseFirestore db = FirebaseFirestore.getInstance();
     protected static FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-    public void addExe(String email, String wo_name, String exe_name, int sets, int reps, double weight_kg) {
+    @SuppressLint({"MissingInflatedId", "SetTextI18n"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        loadContent();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exe_update);
+        email = user.getEmail();
+        UPDATE = findViewById(R.id.updateExe);
+
+        input_reps = findViewById(R.id.reps);
+        input_reps.setText(reps + "");
+
+        input_set = findViewById(R.id.sets);
+        input_set.setText(sets + "");
+
+        input_weight = findViewById(R.id.weight);
+        input_weight.setText(weight + "");
+
+        input_exe = findViewById(R.id.titleExe);
+        input_exe.setText(newScrennW.nameExe);
+
+        UPDATE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String exercise = input_exe.getText().toString();
+                double weight = Double.parseDouble(input_weight.getText().toString());
+                int reps = Integer.parseInt(input_reps.getText().toString());
+                int set = Integer.parseInt(input_set.getText().toString());
+                try {
+                    Gworkout = GroupWorkout.nameTR;
+                    UpdateExe(email, Gworkout, exercise, set, reps, weight);
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+                loadContent();
+                finish();
+
+            }
+        });
+
+    }
+
+    public void loadContent() {
+        db.collection("user-info").document(Objects.requireNonNull(user.getEmail()))
+                .collection("workouts").document(GroupWorkout.nameTR)
+                .collection("exercises").document(newScrennW.nameExe).
+                addSnapshotListener(MetadataChanges.INCLUDE, new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot snapshot, @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        if (snapshot != null && snapshot.exists()) {
+                            exeUpdate.reps = snapshot.getLong("reps");
+                            exeUpdate.sets = snapshot.getLong("sets");
+                            exeUpdate.weight = snapshot.getDouble("weight");
+                            Log.d(TAG, "Current data: " + snapshot.getData());
+                        } else {
+                            Log.d(TAG, "Current data: null");
+                        }
+
+                    }
+
+                });
+
+
+    }
+
+    public void UpdateExe(String email, String wo_name, String exe_name, int sets, int reps, double weight_kg) {
         // create exercise
         Map<String, Object> exe = new HashMap<>();
         exe.put("reps", reps);
@@ -73,84 +146,4 @@ public class exeUpdate extends AppCompatActivity {
                 });
     }
 
-    public void loadContent() {
-        docRef = db.collection("user-info").document(Objects.requireNonNull(user.getEmail()))
-                .collection("workouts").document(GroupWorkout.nameTR)
-                .collection("exercises").document(newScrennW.nameExe);
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    exeUpdate.reps = document.getDouble("reps");
-                    exeUpdate.sets = document.getDouble("sets");
-                    exeUpdate.weight = document.getDouble("weight");
-                    System.out.println(reps);
-                    System.out.println(sets);
-                    System.out.println(weight);
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                    } else {
-                        Log.d(TAG, "No such document");
-                    }
-                } else {
-                    Log.d(TAG, "get failed with ", task.getException());
-                }
-            }
-        });
-
-
-    }
-
-    @SuppressLint({"MissingInflatedId", "WrongViewCast", "SetTextI18n"})
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exe_update);
-        loadContent();
-        email = user.getEmail();
-
-
-        UPDATE = findViewById(R.id.updateExe);
-
-        System.out.println(reps);
-        System.out.println(sets);
-        System.out.println(weight);
-
-        input_reps = findViewById(R.id.reps);
-        input_reps.setText(reps+"");
-
-        input_set = findViewById(R.id.sets);
-        input_set.setText(sets+"");
-
-        input_weight = findViewById(R.id.weight);
-        input_weight.setText(weight+"");
-
-        input_exe = findViewById(R.id.titleExe);
-        input_exe.setText(newScrennW.nameExe);
-
-
-
-
-        UPDATE.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String exercise = input_exe.getText().toString();
-                double weight = Double.parseDouble(input_weight.getText().toString());
-                int reps = Integer.parseInt(input_reps.getText().toString());
-                int set = Integer.parseInt(input_set.getText().toString());
-                try{
-                    Gworkout = GroupWorkout.nameTR;
-                    addExe(email, Gworkout, exercise, set, reps, weight);
-                } catch (NullPointerException e){
-                    e.printStackTrace();
-                }
-                finish();
-
-            }
-        });
-    }
 }
-
-
-// kk@gmail.com
