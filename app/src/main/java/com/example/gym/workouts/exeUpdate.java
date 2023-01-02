f09f35c0152a6ddc38a66887254a6e2ba9ce875c
amitrivka <amitw6363@gmail.com>
2023-01-02 19:04:52 +0200

    Workout

--- app/src/main/java/com/example/gym/workouts/exeUpdate.java
+++ app/src/main/java/com/example/gym/workouts/exeUpdate.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -54,6 +56,9 @@ public class exeUpdate extends AppCompatActivity implements I_updateExercise {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+       getSupportActionBar().hide();
         setContentView(R.layout.activity_exe_update);
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
