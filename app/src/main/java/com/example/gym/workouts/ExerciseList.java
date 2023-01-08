41ccccb410c9f5dfd3426fbcdda3b35434efe407
Talia Seada <talias1235@gmail.com>
2023-01-08 18:34:10 +0200

    connect exerciseList function to firebase-functions

--- app/src/main/java/com/example/gym/workouts/ExerciseList.java
+++ app/src/main/java/com/example/gym/workouts/ExerciseList.java
@@ -2,7 +2,6 @@ package com.example.gym.workouts;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -46,6 +45,20 @@ public class ExerciseList extends AppCompatActivity implements I_exerciseList {
     private String email_trainer = getTrainee.nameTR;
     protected FirebaseFunctions mFunctions = FirebaseFunctions.getInstance();
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        String role = getIntent().getStringExtra("role");
+        String email;
+        if (role.equals("trainee")) {
+            email = email_trainee;
+        } else {
+            email = email_trainer;
+        }
+        // load content from firebase
+        loadContent(email);
+    }
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,6 +88,7 @@ public class ExerciseList extends AppCompatActivity implements I_exerciseList {
             }
         });
 
+
         // set action for the exercises list
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -93,9 +107,6 @@ public class ExerciseList extends AppCompatActivity implements I_exerciseList {
      ***/
     @Override
     public void loadContent(String email) {
-//        db.collection("user-info").document(email)
-//                .collection("workouts").document(WorkoutList.nameTR)
-//                .collection("exercises").
         HashMap<String, String> data = new HashMap<>();
         data.put("email", email);
         data.put("name", WorkoutList.nameTR);
@@ -108,63 +119,31 @@ public class ExerciseList extends AppCompatActivity implements I_exerciseList {
                     items.clear();
                     show.clear();
                     ArrayList<HashMap> data = (ArrayList<HashMap>) task.getResult().getData();
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        data.forEach(e -> {
-                            final Map<String, String> ex = new HashMap<>();
-                            items.add((String) e.get("name"));
-                            String name = (String) e.get("name");
-                            int reps = (int) e.get("reps");
-                            int sets = (int) e.get("sets");
-                            Object weight = e.get("weight");
-                            String time = (String) e.get("time");
-                            String unit = (String) e.get("unit");
-                            ex.put("exercise", name);
-                            ex.put("reps", "Repetition: " + reps);
-                            ex.put("sets", "Sets: " + sets);
-                            ex.put("weight", "Weight: " + weight + " kg");
-                            ex.put("time", "Time: " + time + " " + unit);
-                            show.add(ex);
-                        });
-                    }
+                    data.forEach(e -> {
+                        final Map<String, String> ex = new HashMap<>();
+                        items.add((String) e.get("name"));
+                        String name = (String) e.get("name");
+                        int reps = (int) e.get("reps");
+                        int sets = (int) e.get("sets");
+                        Object weight = e.get("weight");
+                        String time = (String) e.get("time");
+                        String unit = (String) e.get("unit");
+                        ex.put("exercise", name);
+                        ex.put("reps", "Repetition: " + reps);
+                        ex.put("sets", "Sets: " + sets);
+                        ex.put("weight", "Weight: " + weight + " kg");
+                        ex.put("time", "Time: " + time + " " + unit);
+                        show.add(ex);
+                    });
                     String[] from = {"exercise", "reps", "sets", "weight", "time"};
                     int[] to = {R.id.exe, R.id.reps_e, R.id.sets_e, R.id.weight_e, R.id.time_e};
                     adap = new SimpleAdapter(ExerciseList.this, show, R.layout.exe_item, from, to);
                     listView.setAdapter(adap);
-                }
-                else{
+                } else {
                     Log.w("Get Exercises", task.getException());
                 }
             }
         });
-//                addSnapshotListener(new EventListener<QuerySnapshot>() {
-//            @Override
-//            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
-//                items.clear();
-//                show.clear();
-//                for (DocumentSnapshot snapshot : documentSnapshots) {
-//                    final Map<String, String> ex = new HashMap<>();
-//                    items.add(snapshot.getString("name"));
-//                    String name = snapshot.getString("name");
-//                    Long reps = snapshot.getLong("reps");
-//                    Long sets = snapshot.getLong("sets");
-//                    double weight = snapshot.getDouble("weight");
-//                    String time = snapshot.getString("time");
-//                    String unit = snapshot.getString("unit");
-////                            exe_object eo = new exe_object(name, reps, sets, weight);
-//                    ex.put("exercise", name);
-//                    ex.put("reps", "Repetition: " + String.valueOf(reps));
-//                    ex.put("sets", "Sets: " + String.valueOf(sets));
-//                    ex.put("weight", "Weight: " + String.valueOf(weight) + " kg");
-//                    ex.put("time", "Time: " + time + " " + unit);
-//                    show.add(ex);
-//                }
-//                String[] from = {"exercise", "reps", "sets", "weight", "time"};
-//                int[] to = {R.id.exe, R.id.reps_e, R.id.sets_e, R.id.weight_e, R.id.time_e};
-////                        adap = new SimpleAdapter(ExerciseList.this, show, R.layout.grid_layout, from, to);
-//                adap = new SimpleAdapter(ExerciseList.this, show, R.layout.exe_item, from, to);
-//                listView.setAdapter(adap);
-//            }
-//        });
     }
 
     /***
